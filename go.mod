e95136577700e2de6b05abc0315d183475a45a93
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-01-24 12:42:13 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module simplecli
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// The "main" function is the entrypoint of a Go Program
+func main() {
+	//Add fmt to declare from where Print is coming from
+	fmt.Print("Hello World")
+	// everything is organized into packages
+	// In this case, we are using the built-in "fmt" package which contains functions
+}
