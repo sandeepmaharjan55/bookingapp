841e460a6f3ebbb6fd2793190deb48752e1d190e
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-01-24 15:03:06 -0800

    some data types

--- main.go
+++ main.go
@@ -4,20 +4,24 @@ import "fmt"
 
 // The "main" function is the entrypoint of a Go Program
 func main() {
-	//Add fmt to declare from where Print is coming from
-	//fmt.Print returns Hello World%
-	//fmt.Print("Hello World")
+	//use of variables in Go
+	var confName = "Go to the Conference"
+	//var confName string = "Go to the Conference"
+	const confTickets = 100
+	var remTickets = 50
+	confNameTrial := "Go to the Conference"
+	// if here confTickets = 50 is done it will not change the value of confTickets as it is defined as const.
 
-	//fmt.Println returns Hello World in new line
-	//println prints in a new line
 	fmt.Println("Welcome to this booking application!!")
 	fmt.Println("Get your tickets from here.")
-	// everything is organized into packages
-	// In this case, we are using the built-in "fmt" package which contains functions
 
-	//use of variables in Go
-	var confName = "Go to the Conference."
 	fmt.Println(confName)
+	fmt.Println("Welcome to", confName, "booking application!!")
+	fmt.Println("Welcome to Trial ", confNameTrial, "booking application!!")
+	fmt.Println("We have total of ", confTickets, "tickets and ", remTickets, " are remaining")
 
-	//end of use of variables in Go
+	// use of print formatted data
+	fmt.Printf("Welcome to %v booking application!! \n", confName)
+	fmt.Printf("We have total of %v tickets and %v are remaining", confTickets, remTickets)
+
 }
