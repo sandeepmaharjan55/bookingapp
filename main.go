4406603c927391649ffcd53e55b66b581e2eceeb
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-01-25 11:57:35 -0800

    adding for - for multiple bookings

--- main.go
+++ main.go
@@ -9,39 +9,49 @@ func main() {
 	//var confName string = "Go to the Conference"
 	const confTickets int = 100
 	var remTickets uint = 50
-	confNameTrial := "Go to the Conference"
+	// confNameTrial := "Go to the Conference"
 	// if here confTickets = 50 is done it will not change the value of confTickets as it is defined as const.
 	bookings := []string{}
 
-	fmt.Println("Welcome to this booking application!!")
-	fmt.Println("Get your tickets from here.")
+	// fmt.Println("Welcome to this booking application!!")
+	// fmt.Println("Get your tickets from here.")
 
-	fmt.Println(confName)
-	fmt.Println("Welcome to", confName, "booking application!!")
-	fmt.Println("Welcome to Trial ", confNameTrial, "booking application!!")
-	fmt.Println("We have total of ", confTickets, "tickets and ", remTickets, " are remaining")
+	// fmt.Println(confName)
+	// fmt.Println("Welcome to", confName, "booking application!!")
+	// fmt.Println("Welcome to Trial ", confNameTrial, "booking application!!")
+	// fmt.Println("We have total of ", confTickets, "tickets and ", remTickets, " are remaining")
 
 	// use of print formatted data
 	fmt.Printf("Welcome to %v booking application!! \n", confName)
+	fmt.Println("Get your tickets from here.")
 	fmt.Printf("We have total of %v tickets and %v are remaining", confTickets, remTickets)
 
-	var userName string
-	var userTickets uint
+	for {
+		var userFirstName string
+		var userLastName string
+		var userTickets uint
+		var email string
 
-	//uint for positive number of tickets.
-	fmt.Println("\n Your Name?")
-	fmt.Scan(&userName)
-	fmt.Println("\nHello ", userName, " How many tickets you want?")
-	fmt.Scan(&userTickets)
-	fmt.Println("\nNumber of tickets", userTickets)
+		//uint for positive number of tickets.
+		fmt.Println("\n Enter your first name: ")
+		fmt.Scan(&userFirstName)
+		fmt.Println("\n Enter your last name: ")
+		fmt.Scan(&userLastName)
+		fmt.Println("\n Enter your email id: ")
+		fmt.Scan(&email)
+		fmt.Println("\nHello ", userFirstName, "!!\nHow many tickets do you want?")
+		fmt.Scan(&userTickets)
+		fmt.Println("\nEnter the number of tickets", userTickets)
 
-	//arrays
-	remTickets = remTickets - userTickets
-	bookings = append(bookings, userName)
-	// var bookings = []string{"Sandy", "Mandy", "Handy", "Katappa"}
-	// for i := 0; i < len(bookings); i++ {
-	// 	fmt.Println(i+1, ".", bookings[i])
-	// }
-	fmt.Printf("\nThank you for booking %v tickets. Now remaining tickets are %v", userTickets, remTickets)
-	fmt.Printf("\nThese are all bookings %v", bookings)
+		//arrays
+		remTickets = remTickets - userTickets
+		bookings = append(bookings, userFirstName+" "+userLastName)
+		// var bookings = []string{"Sandy", "Mandy", "Handy", "Katappa"}
+		// for i := 0; i < len(bookings); i++ {
+		// 	fmt.Println(i+1, ".", bookings[i])
+		// }
+		fmt.Printf("\nThank you for booking %v tickets. Confirmation is sent to %v", userTickets, email)
+		fmt.Printf("\n Now remaining tickets are %v", remTickets)
+		fmt.Printf("\nThese are all bookings %v \n", bookings)
+	}
 }
